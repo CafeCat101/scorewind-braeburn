21baacef603dc089460a09e9b2766006107c15e5
Leonore <leonore.yardimli@icloud.com>
2022-07-01 23:37:17 +0800

    Initial Commit

---
+++ ScoreWind/ScoreWindApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreWindApp.swift
+//  ScoreWind
+//
+//  Created by Leonore Yardimli on 2022/7/1.
+//
+
+import SwiftUI
+
+@main
+struct ScoreWindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
