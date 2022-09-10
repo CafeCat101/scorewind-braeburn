d5497640def6f1eb51f99b1046fe1e1d4bb462a4
Leonore <leonore.yardimli@icloud.com>
2022-09-10 13:35:19 +0800

    added a little visual clue to the wizard idea

---
+++ ScoreWind/Types/WizardStep.swift
@@ -0,0 +1,13 @@
+//
+//  WizardStep.swift
+//  ScoreWind
+//
+//  Created by Leonore Yardimli on 2022/9/10.
+//
+
+import Foundation
+
+enum WizardStep {
+	case chooseInstrument
+	case scorePlayable
+}
--- ScoreWind/WizardView.swift
+++ ScoreWind/WizardView.swift
@@ -10,56 +10,98 @@ import SwiftUI
 struct WizardView: View {
 	@EnvironmentObject var scorewindData:ScorewindData
 	@Binding var selectedTab:String
+	@State private var userRole = "student"
+	@State private var stepName:WizardStep = .chooseInstrument
 	
 	var body: some View {
 		VStack {
-			/*HStack {
-				Spacer()
-				Label("Scorewind", systemImage: "music.note")
-						.labelStyle(.titleAndIcon)
-				Spacer()
-			}
-			.padding()
-			.background(Color("ScreenTitleBg"))*/
 			Label("Scorewind", systemImage: "music.note")
 				.labelStyle(.titleAndIcon)
 				.contextMenu {
 					Button(action: {
-						scorewindData.studentData.removeAKey(keyName: "completedLessons")
+						userRole = "student"
 					}){
-						Label("Clear all completed lesson data", systemImage: "xmark.circle")
+						Label("I'm a student", systemImage: "face.smiling")
+							.labelStyle(.titleAndIcon)
+					}
+					Button(action: {
+						userRole = "teacher"
+					}){
+						Label("Teachers only", systemImage: "brain.head.profile")
 							.labelStyle(.titleAndIcon)
 					}
 				}
-		
-			List {
-				Section(header: Text("All")) {
-					ForEach(scorewindData.allCourses) { course in
-						Button(action: {
-							scorewindData.currentCourse = course
-							scorewindData.currentView = Page.course
-							self.selectedTab = "TCourse"
-							scorewindData.currentLesson = scorewindData.currentCourse.lessons[0]
-							scorewindData.setCurrentTimestampRecs()
-							//scorewindData.lastViewAtScore = true
-							scorewindData.lastPlaybackTime = 0.0
-						}) {
-							if course.id == scorewindData.currentCourse.id {
-								Text(scorewindData.replaceCommonHTMLNumber(htmlString: course.title))
-									.foregroundColor(Color("WizardListTextHighlight"))
-							} else {
-								Text(scorewindData.replaceCommonHTMLNumber(htmlString: course.title))
-									.foregroundColor(Color("MyCourseItemText"))
+			
+			if userRole == "teacher" {
+				List {
+					Section(header: Text("All")) {
+						ForEach(scorewindData.allCourses) { course in
+							Button(action: {
+								scorewindData.currentCourse = course
+								scorewindData.currentView = Page.course
+								self.selectedTab = "TCourse"
+								scorewindData.currentLesson = scorewindData.currentCourse.lessons[0]
+								scorewindData.setCurrentTimestampRecs()
+								//scorewindData.lastViewAtScore = true
+								scorewindData.lastPlaybackTime = 0.0
+							}) {
+								if course.id == scorewindData.currentCourse.id {
+									Text(scorewindData.replaceCommonHTMLNumber(htmlString: course.title))
+										.foregroundColor(Color("WizardListTextHighlight"))
+								} else {
+									Text(scorewindData.replaceCommonHTMLNumber(htmlString: course.title))
+										.foregroundColor(Color("MyCourseItemText"))
+								}
+								
 							}
-							
 						}
 					}
 				}
+				.listStyle(GroupedListStyle())
+				.background(Color("LessonListTextBg"))
+			} else {
+				if stepName == .chooseInstrument {
+					Spacer()
+					Text("Which instrument do you want to play?")
+						.font(.headline)
+					
+					HStack {
+						Button(action:{
+							
+						}){
+							Circle()
+								.strokeBorder(Color.black,lineWidth: 1)
+								.background(Circle().foregroundColor(Color.white))
+								.frame(width:100,height:100)
+								.overlay(
+									Image("instrument-guitar-icon")
+										.resizable()
+										.scaleEffect(0.6)
+								)
+						}
+						
+						Button(action:{
+							
+						}){
+							Circle()
+								.strokeBorder(Color.black,lineWidth: 1)
+								.background(Circle().foregroundColor(Color.white))
+								.frame(width:100,height:100)
+								.overlay(
+									Image("instrument-violin-icon")
+										.resizable()
+										.scaleEffect(0.6)
+								)
+						}
+					}
+					
+					Spacer()
+				}
 			}
-			.listStyle(GroupedListStyle())
-			.background(Color("LessonListTextBg"))
-		}//.background(Color("ScreenTitleBg"))
-		
+			
+			
+			
+		}
 	}
 }
 
