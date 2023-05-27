db4c9d81ce1bd99f4828ffd49ceb5a642cfa2bd3
CafeCat101 <leonore.yardimli@icloud.com>
2023-05-27 14:47:51 +0800

    revised the information in StoreView when users cancel it

--- ScoreWind/StatusInfoView.swift
+++ ScoreWind/StatusInfoView.swift
@@ -6,13 +6,82 @@ struct StatusInfoView: View {
 	
 	let product: Product
 	let status: Product.SubscriptionInfo.Status
+	@Environment(\.verticalSizeClass) var verticalSize
+	@Environment(\.colorScheme) var colorScheme
 	
 	var body: some View {
-		Text(statusDescription())
+		/*Text(statusDescription())
 			.bold()
 			.foregroundColor(Color("Dynamic/MainBrown+6"))
 			.multilineTextAlignment(.center)
-			.frame(maxWidth: .infinity, alignment: .center)
+			.frame(maxWidth: .infinity, alignment: .center)*/
+		
+		VStack {
+			(Text(product.displayPrice)+Text(" / ")+Text(getFriendlyPeriodName(product.subscription!, isIntroduction: false)))
+				.bold()
+				//.padding(.bottom, 20)
+			if isInTrial() {
+				HStack(spacing:0) {
+					Spacer()
+					/*Image(systemName: "gift.fill")
+						.resizable()
+						.scaledToFit()
+						.frame(maxWidth: 25)
+						.padding(.trailing,5)*/
+					Text("1-month free trial")
+						.bold()
+						.padding([.top,.bottom], 5)
+						.multilineTextAlignment(.center)
+					Spacer()
+				}
+			}
+		}
+		
+		if status.state == .subscribed {
+			HStack {
+				if colorScheme == .light {
+					Text("Subscribed")
+						.fontWeight(Font.Weight.bold)
+						.foregroundColor(Color("Dynamic/ShadowReverse"))
+				} else {
+					Text("Subscribed")
+						.foregroundColor(Color("Dynamic/StoreViewTitle"))
+						.fontWeight(Font.Weight.medium)
+				}
+				
+				Text(Image(systemName: "checkmark"))
+					.bold()
+					.foregroundColor(Color("Dynamic/ShadowReverse"))
+			}
+			.frame(maxWidth: verticalSize == .regular ? UIScreen.main.bounds.size.width*0.7 : UIScreen.main.bounds.size.width*0.5)
+			.padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 15))
+			.background(
+				RoundedRectangle(cornerRadius: CGFloat(17))
+					.foregroundColor(colorScheme == .light ? Color("Dynamic/StoreViewTitle") : Color("Dynamic/MainBrown"))
+					.shadow(color: Color("Dynamic/Shadow"),radius: CGFloat(5))
+					.opacity(colorScheme == .light ? 1 : 0.25)
+					.overlay {
+						RoundedRectangle(cornerRadius: 17)
+							.stroke(Color("Dynamic/DarkGray"), lineWidth: 1)
+					}
+			)
+		}
+		
+		if isInTrial() {
+			Text("Yoy will not be charged during your trial.")
+				.foregroundColor(Color("Dynamic/MainBrown+6"))
+				.bold()
+				.padding([.leading,.trailing,.top], 30)
+				.font(.subheadline)
+		}
+		
+		Text(statusDescription())
+			.foregroundColor(Color("Dynamic/MainBrown+6"))
+			.bold()
+			.padding([.leading,.trailing], 30)
+			.padding(.top, isInTrial() ? 0 : 30)
+			.font(.subheadline)
+		
 	}
 	
 	private func isInTrial() -> Bool {
@@ -45,17 +114,20 @@ struct StatusInfoView: View {
 		}
 		
 		if transaction.offerType != nil {
-			print("transaction.offerType: \(String(describing: transaction.offerType))")
+			print("[debug]transaction.offerType: \(String(describing: transaction.offerType))")
+			print("[debug]renewalInfo.offerType: \(String(describing: renewalInfo.offerType))")
 		}else {
-			print("no offerType")
+			print("[debug]no offerType")
 		}
-		print("\(String(describing: transaction.offerType))")
+		print("transaction.offerType - \(String(describing: transaction.offerType))")
+		print("renewalInfo.offerType - \(String(describing: renewalInfo.offerType))")
+		print("\(String(describing: transaction.expirationDate))")
 		
 		var description = ""
 		
 		switch status.state {
 		case .subscribed:
-			description = subscribedDescription()
+			description = ""//subscribedDescription()
 		case .expired:
 			if let expirationDate = transaction.expirationDate,
 				 let expirationReason = renewalInfo.expirationReason {
@@ -76,7 +148,49 @@ struct StatusInfoView: View {
 		if let expirationDate = transaction.expirationDate {
 			description += renewalDescription(renewalInfo, expirationDate)
 		}
+		
+		if description.isEmpty && status.state == .subscribed && renewalInfo.willAutoRenew == false {
+			//:: no renew date but subscribed, user has cancelled.
+			description += "You can still access \(product.displayName) until \(transaction.expirationDate!.storeFormattedDate())."
+		}
 		return description
+	}
+	
+	private func getFriendlyPeriodName(_ subscription: Product.SubscriptionInfo, isIntroduction: Bool) -> String {
+		let unit: String
+		if isIntroduction {
+			let plural = 1 < subscription.introductoryOffer?.period.value ?? 0
+			switch subscription.introductoryOffer?.period.unit {
+			case .day:
+				unit = plural ? "\(subscription.subscriptionPeriod.value) days" : "day"
+			case .week:
+				unit = plural ? "\(subscription.subscriptionPeriod.value) weeks" : "week"
+			case .month:
+				unit = plural ? "\(subscription.subscriptionPeriod.value) months" : "month"
+			case .year:
+				unit = plural ? "\(subscription.subscriptionPeriod.value) years" : "year"
+			case .none:
+				unit = ""
+			@unknown default:
+				unit = ""
+			}
+		} else {
+			let plural = 1 < subscription.subscriptionPeriod.value
+			switch subscription.subscriptionPeriod.unit {
+			case .day:
+				unit = plural ? "\(subscription.subscriptionPeriod.value) days" : "day"
+			case .week:
+				unit = plural ? "\(subscription.subscriptionPeriod.value) weeks" : "week"
+			case .month:
+				unit = plural ? "\(subscription.subscriptionPeriod.value) months" : "month"
+			case .year:
+				unit = plural ? "\(subscription.subscriptionPeriod.value) years" : "year"
+			@unknown default:
+				unit = "period"
+			}
+		}
+		
+		return unit
 	}
 	
 	fileprivate func billingRetryDescription() -> String {
@@ -103,8 +217,9 @@ struct StatusInfoView: View {
 		
 		if let newProductID = renewalInfo.autoRenewPreference {
 			if let newProduct = store.subscriptions.first(where: { $0.id == newProductID }) {
-				description += "\nYour subscription to \(newProduct.displayName)"
-				description += " will begin when your current subscription expires on \(expirationDate.storeFormattedDate())."
+				//description += "\nYour subscription to \(newProduct.displayName)"
+				//description += " will begin when your current subscription expires on \(expirationDate.storeFormattedDate())."
+				description += "\(newProduct.displayName) renews \(expirationDate.storeFormattedDate())"
 			}
 		} else if renewalInfo.willAutoRenew {
 			description += "\nNext billing date: \(expirationDate.storeFormattedDate())."
@@ -163,7 +278,7 @@ struct StatusInfoView_Previews: PreviewProvider {
 extension Date {
 	func storeFormattedDate() -> String {
 		let dateFormatter = DateFormatter()
-		dateFormatter.dateFormat = "MMM dd, yyyy"
+		dateFormatter.dateFormat = "MMM dd, yyyy h:mm a"
 		return dateFormatter.string(from: self)
 	}
 }
