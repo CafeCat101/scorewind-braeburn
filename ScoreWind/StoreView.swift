41145bf0ffa485ccc0e279fffd8bc53346524d96
CafeCat <leonore1982@gmail.com>
2023-07-18 17:59:34 +0800

    commit test

--- ScoreWind/StoreView.swift
+++ ScoreWind/StoreView.swift
@@ -21,7 +21,7 @@ struct StoreView: View {
 	//@State private var offerIntroduction = false
 	//@State private var enableBuyButton = true
 	@State private var studentCouponCode:String = ""
-	@State private var showCouponWaiting = 0
+	@State private var showCouponWaiting = 0 //test
 	
 	var availableSubscriptions: [Product] {
 		store.subscriptions.filter { $0.id != currentSubscription?.id }
