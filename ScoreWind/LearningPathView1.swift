4c169150089c063b10d53907a6d6847c6d93285c
CafeCat101 <leonore.yardimli@icloud.com>
2023-09-23 19:50:49 +0800

    Save before make changes

---
+++ ScoreWind/LearningPathView1.swift
@@ -0,0 +1,20 @@
+//
+//  LearningPathView1.swift
+//  ScoreWind
+//
+//  Created by Leonore Yardimli on 2023/9/14.
+//
+
+import SwiftUI
+
+struct LearningPathView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LearningPathView1_Previews: PreviewProvider {
+    static var previews: some View {
+        LearningPathView1()
+    }
+}
